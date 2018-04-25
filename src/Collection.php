359d7acd4df03f054c516dc0422384af1c6eadef
Nikolaj Boel Jensen <nbj@cego.dk>
2018-04-25 22:26:58 +0200

    Initial commit

---
+++ src/Collection.php
@@ -0,0 +1,391 @@
+<?php
+
+namespace Nbj;
+
+use Countable;
+use ArrayAccess;
+use RuntimeException;
+use ReflectionFunction;
+use ReflectionException;
+
+class Collection implements ArrayAccess, Countable
+{
+    /**
+     * Holds all the items in the collection
+     *
+     * @var array $items
+     */
+    protected $items = [];
+
+    /**
+     * Named construct for creating a new collection
+     *
+     * @param array $items
+     *
+     * @return static
+     */
+    public static function make($items = [])
+    {
+        return new static($items);
+    }
+
+    /**
+     * Collection constructor.
+     *
+     * @param array $items
+     */
+    public function __construct($items = [])
+    {
+        if (!is_array($items)) {
+            $items = [$items];
+        }
+
+        $this->items = $items;
+    }
+
+    /**
+     * Pushes an item to the collection
+     *
+     * @param mixed $item
+     *
+     * @return $this
+     */
+    public function push($item)
+    {
+        array_push($this->items, $item);
+
+        return $this;
+    }
+
+    /**
+     * Pops off the last item in the collection
+     *
+     * @return mixed
+     */
+    public function pop()
+    {
+        return array_pop($this->items);
+    }
+
+    /**
+     * Shifts off the first item in the collection
+     *
+     * @return mixed
+     */
+    public function shift()
+    {
+        return array_shift($this->items);
+    }
+
+    /**
+     * Adds an item to the collection
+     * syntactic sugar for pushing to
+     * the collection
+     *
+     * @param mixed $item
+     *
+     * @return $this
+     */
+    public function add($item)
+    {
+        return $this->push($item);
+    }
+
+    /**
+     * Checks if the collection is empty
+     *
+     * @return bool
+     */
+    public function isEmpty()
+    {
+        return empty($this->items);
+    }
+
+    /**
+     * Syntactic sugar for checking if the
+     * collection is not empty
+     *
+     * @return bool
+     */
+    public function isNotEmpty()
+    {
+        return !$this->isEmpty();
+    }
+
+    /**
+     * Returns the first item of the collection
+     *
+     * @return mixed
+     */
+    public function first()
+    {
+        return reset($this->items);
+    }
+
+    /**
+     * Returns the last item of the collection
+     *
+     * @return mixed
+     */
+    public function last()
+    {
+        return end($this->items);
+    }
+
+    /**
+     * Iterates over each item in the collection
+     *
+     * @param callable $callback
+     *
+     * @return $this
+     */
+    public function each(callable $callback)
+    {
+        array_walk($this->items, $callback);
+
+        return $this;
+    }
+
+    /**
+     * Maps over each item in the collection
+     *
+     * @param callable $callback
+     *
+     * @return Collection
+     */
+    public function map(callable $callback)
+    {
+        return new Collection(array_map($callback, $this->items));
+    }
+
+    /**
+     * Applies a filter to the collection
+     *
+     * @param callable $callback
+     *
+     * @return Collection
+     *
+     * @throws ReflectionException
+     * @throws RuntimeException
+     */
+    public function filter(callable $callback)
+    {
+        $reflectionMethod = new ReflectionFunction($callback);
+        $numberOfParameters = $reflectionMethod->getNumberOfParameters();
+
+        if ($numberOfParameters == 1) {
+            return new Collection(array_filter($this->items, $callback));
+        }
+
+        if ($numberOfParameters == 2) {
+            return new Collection(array_filter($this->items, $callback, ARRAY_FILTER_USE_BOTH));
+        }
+
+        throw new RuntimeException('Too many parameters for filter() callback');
+    }
+
+    /**
+     * Rejects items in an array
+     *
+     * @param callable $callback
+     *
+     * @return Collection
+     *
+     * @throws ReflectionException
+     * @throws RuntimeException
+     */
+    public function reject(callable $callback)
+    {
+        $reflectionMethod = new ReflectionFunction($callback);
+        $numberOfParameters = $reflectionMethod->getNumberOfParameters();
+
+        if ($numberOfParameters == 1) {
+            return $this->filter(function ($item) use ($callback) {
+                return !$callback($item);
+            });
+        }
+
+        if ($numberOfParameters == 2) {
+            return $this->filter(function ($value, $key) use ($callback) {
+                return !$callback($value, $key);
+            });
+        }
+
+        throw new RuntimeException('Too many parameters for reject() callback');
+    }
+
+    /**
+     * Performs a truth test on each item in the collection
+     *
+     * @param callable $callback
+     *
+     * @return bool
+     *
+     * @throws ReflectionException
+     */
+    public function every(callable $callback)
+    {
+        $filteredCollection = $this->filter($callback);
+
+        return $filteredCollection->count() == $this->count();
+    }
+
+    /**
+     * It can reduce a collection to a single value
+     *
+     * @param callable $callback
+     * @param mixed $initial
+     *
+     * @return mixed
+     */
+    public function reduce(callable $callback, $initial)
+    {
+        return array_reduce($this->items, $callback, $initial);
+    }
+
+    /**
+     * Summarizes the collection
+     *
+     * @param mixed $field
+     *
+     * @return mixed
+     */
+    public function sum($field = null)
+    {
+        // Assume the collection is index based and contains only numbers
+        if (is_null($field)) {
+            return array_sum($this->items);
+        }
+
+        // Assume the collection contains arrays or objects
+        return $this->reduce(function ($carry, $item) use ($field) {
+            if (is_array($item)) {
+                $carry += $item[$field];
+            }
+
+            if (is_object($item)) {
+                $carry += $item->$field;
+            }
+
+            return $carry;
+        }, 0);
+    }
+
+    /**
+     * Converts the collection to an array
+     * by returning the underlying array
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->items;
+    }
+
+    /**
+     * Converts the collection to json
+     *
+     * @param int $options
+     *
+     * @return string
+     */
+    public function toJson($options = 0)
+    {
+        return json_encode($this->items, $options);
+    }
+
+    /**
+     * Whether a offset exists
+     *
+     * @link http://php.net/manual/en/arrayaccess.offsetexists.php
+     *
+     * @param mixed $offset <p>
+     * An offset to check for.
+     * </p>
+     *
+     * @return bool true on success or false on failure.
+     * </p>
+     * <p>
+     * The return value will be casted to boolean if non-boolean was returned.
+     *
+     * @since 5.0.0
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->items[$offset]);
+    }
+
+    /**
+     * Offset to retrieve
+     *
+     * @link http://php.net/manual/en/arrayaccess.offsetget.php
+     *
+     * @param mixed $offset <p>
+     * The offset to retrieve.
+     * </p>
+     *
+     * @return mixed Can return all value types.
+     *
+     * @since 5.0.0
+     */
+    public function offsetGet($offset)
+    {
+        return $this->items[$offset];
+    }
+
+    /**
+     * Offset to set
+     *
+     * @link http://php.net/manual/en/arrayaccess.offsetset.php
+     *
+     * @param mixed $offset <p>
+     * The offset to assign the value to.
+     * </p>
+     * @param mixed $value <p>
+     * The value to set.
+     * </p>
+     *
+     * @return void
+     *
+     * @since 5.0.0
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->items[$offset] = $value;
+    }
+
+    /**
+     * Offset to unset
+     *
+     * @link http://php.net/manual/en/arrayaccess.offsetunset.php
+     *
+     * @param mixed $offset <p>
+     * The offset to unset.
+     * </p>
+     *
+     * @return void
+     *
+     * @since 5.0.0
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->items[$offset]);
+    }
+
+    /**
+     * Count elements of an object
+     *
+     * @link http://php.net/manual/en/countable.count.php
+     *
+     * @return int The custom count as an integer.
+     * </p>
+     * <p>
+     * The return value is cast to an integer.
+     *
+     * @since 5.1.0
+     */
+    public function count()
+    {
+        return count($this->items);
+    }
+}
---
+++ tests/Feature/CollectionsTest.php
@@ -0,0 +1,479 @@
+<?php
+namespace Tests;
+
+use Countable;
+use Exception;
+use ArrayAccess;
+use Nbj\Collection;
+use RuntimeException;
+use PHPUnit\Framework\TestCase;
+
+class CollectionsTest extends TestCase
+{
+    /** @test */
+    public function it_is_an_instance_of_array_access()
+    {
+        $collection = new Collection;
+
+        $this->assertInstanceOf(ArrayAccess::class, $collection);
+    }
+
+    /** @test */
+    public function it_behaves_like_an_array()
+    {
+        $collection = new Collection;
+        $this->assertEquals(0, count($collection));
+
+        $collection->add('some-item');
+        $this->assertEquals(1, count($collection));
+
+        $this->assertEquals('some-item', $collection[0]);
+
+        unset($collection[0]);
+        $this->assertEquals(0, count($collection));
+
+        $collection['some-key'] = 'some-value';
+        $this->assertEquals(1, count($collection));
+        $this->assertTrue(isset($collection['some-key']));
+    }
+
+    /** @test */
+    public function it_is_an_instance_of_countable()
+    {
+        $collection = new Collection;
+
+        $this->assertInstanceOf(Countable::class, $collection);
+    }
+
+    /** @test */
+    public function it_has_a_named_constructor_by_the_name_of_make()
+    {
+        $collection = Collection::make('some-item');
+
+        $this->assertCount(1, $collection);
+    }
+
+    /** @test */
+    public function it_can_have_new_items_pushed_to_it()
+    {
+        $collection = new Collection;
+        $this->assertEmpty($collection);
+
+        $collection->push('some_item');
+        $this->assertCount(1, $collection);
+    }
+
+    /** @test */
+    public function it_can_be_initialized_with_a_single_item()
+    {
+        $collection = new Collection('some-item');
+
+        $this->assertCount(1, $collection);
+    }
+
+    /** @test */
+    public function it_can_be_initialized_with_multiple_item()
+    {
+        $collection = new Collection([
+            'some_item',
+            'some_other_item',
+            'some_third_item'
+        ]);
+
+        $this->assertCount(3, $collection);
+    }
+
+    /** @test */
+    public function it_can_have_new_items_added_to_it()
+    {
+        $collection = new Collection;
+        $this->assertEmpty($collection);
+
+        $collection->add('some_item');
+        $this->assertCount(1, $collection);
+    }
+
+    /** @test */
+    public function it_knows_if_it_is_empty()
+    {
+        $collection = new Collection;
+
+        $this->assertTrue($collection->isEmpty());
+
+        $collection->add('some_item');
+        $collection->add('some_other_item');
+
+        $this->assertFalse($collection->isEmpty());
+    }
+
+    /** @test */
+    public function it_knows_if_it_is_not_empty()
+    {
+        $collection = new Collection;
+
+        $this->assertFalse($collection->isNotEmpty());
+
+        $collection->add('some_item');
+        $collection->add('some_other_item');
+
+        $this->assertTrue($collection->isNotEmpty());
+    }
+
+    /** @test */
+    public function it_can_convert_it_self_to_an_array()
+    {
+        $collection = new Collection;
+        $collection->add('some_item');
+        $collection->add('some_other_item');
+        $this->assertInstanceOf(Collection::class, $collection);
+
+        $collection = $collection->toArray();
+
+        $this->assertTrue(is_array($collection));
+    }
+
+    /** @test */
+    public function it_can_convert_it_self_to_json()
+    {
+        $collection = new Collection([
+            'name' => 'john',
+            'age'  => 35
+        ]);
+
+        $json = $collection->toJson();
+
+        $this->assertEquals('{"name":"john","age":35}', $json);
+    }
+
+    /** @test */
+    public function it_knows_how_many_items_it_stores()
+    {
+        $collection = new Collection;
+        $collection->add('some_item');
+        $collection->add('some_other_item');
+
+        $this->assertEquals(2, $collection->count());
+    }
+
+    /** @test */
+    public function it_can_pop_off_the_last_item_of_the_collection()
+    {
+        $collection = new Collection;
+        $collection->add('some_item');
+        $collection->add('some_other_item');
+        $this->assertCount(2, $collection);
+
+        $item = $collection->pop();
+
+        $this->assertCount(1, $collection);
+        $this->assertEquals('some_other_item', $item);
+    }
+
+    /** @test */
+    public function it_can_shift_off_the_first_item_of_the_collection()
+    {
+        $collection = new Collection;
+        $collection->add('some_item');
+        $collection->add('some_other_item');
+        $this->assertCount(2, $collection);
+
+        $item = $collection->shift();
+
+        $this->assertCount(1, $collection);
+        $this->assertEquals('some_item', $item);
+    }
+
+    /** @test */
+    public function it_can_get_the_first_item_of_the_collection()
+    {
+        $collection = new Collection;
+        $collection->add('some_item');
+        $collection->add('some_other_item');
+        $collection->add('some_third_item');
+
+        $item = $collection->first();
+
+        $this->assertCount(3, $collection);
+        $this->assertEquals('some_item', $item);
+    }
+
+    /** @test */
+    public function it_can_get_the_last_item_of_the_collection()
+    {
+        $collection = new Collection;
+        $collection->add('some_item');
+        $collection->add('some_other_item');
+        $collection->add('some_third_item');
+
+        $item = $collection->last();
+
+        $this->assertCount(3, $collection);
+        $this->assertEquals('some_third_item', $item);
+    }
+
+    /** @test */
+    public function it_can_iterate_over_each_item_in_the_collection()
+    {
+        $collection = new Collection;
+        $collection->add('some_item');
+        $collection->add('some_other_item');
+        $collection->add('some_third_item');
+
+        $counter = 0;
+
+        $collection->each(function ($item) use (&$counter) {
+            $haystack = [
+                'some_item',
+                'some_other_item',
+                'some_third_item'
+            ];
+
+            $this->assertTrue(in_array($item, $haystack));
+            $counter++;
+        });
+
+        $this->assertEquals(3, $counter);
+    }
+
+    /** @test */
+    public function it_can_iterate_over_each_item_in_the_collection_with_both_key_and_value()
+    {
+        $collection = new Collection([
+            'name' => 'john',
+            'age'  => 35,
+            'sex'  => 'male'
+        ]);
+
+        $collection->each(function ($value, $key) use ($collection) {
+            $this->assertTrue(array_key_exists($key, $collection->toArray()));
+            $this->assertTrue(in_array($value, $collection->toArray()));
+            $this->assertTrue($collection[$key] == $value);
+        });
+    }
+
+    /** @test */
+    public function it_can_map_over_each_item_in_the_collection()
+    {
+        $collection = new Collection;
+        $collection->add('some_item');
+        $collection->add('some_other_item');
+        $collection->add('some_third_item');
+
+        $newCollection = $collection->map(function ($item) {
+            return $item . '_mapped';
+        });
+
+        $this->assertCount(3, $newCollection);
+        $this->assertInstanceOf(Collection::class, $newCollection);
+
+        $item = $newCollection->shift();
+        $this->assertEquals('some_item_mapped', $item);
+
+        $item = $newCollection->shift();
+        $this->assertEquals('some_other_item_mapped', $item);
+
+        $item = $newCollection->shift();
+        $this->assertEquals('some_third_item_mapped', $item);
+    }
+
+    /** @test */
+    public function it_can_filter_the_collection()
+    {
+        $collection = new Collection([
+            'some_item',
+            'some_other_item',
+            'some_third_item'
+        ]);
+
+        $newCollection = $collection->filter(function ($item) {
+            return $item == 'some_other_item';
+        });
+
+        $this->assertCount(1, $newCollection);
+
+        $item = $newCollection->first();
+        $this->assertEquals('some_other_item', $item);
+    }
+
+    /** @test */
+    public function it_can_reject_items_in_the_collection()
+    {
+        $collection = new Collection([
+            'some_item',
+            'some_other_item',
+            'some_third_item'
+        ]);
+
+        $newCollection = $collection->reject(function ($item) {
+            return $item == 'some_other_item';
+        });
+
+        $this->assertCount(2, $newCollection);
+
+        $item = $newCollection->shift();
+        $this->assertEquals('some_item', $item);
+
+        $item = $newCollection->shift();
+        $this->assertEquals('some_third_item', $item);
+    }
+
+    /** @test */
+    public function a_filter_accepts_both_key_and_value()
+    {
+        $collection = new Collection([
+            'name' => 'john',
+            'age'  => 35,
+            'sex'  => 'male'
+        ]);
+
+        $newCollection = $collection->filter(function ($value, $key) {
+            return $key == 'name' && $value == 'john';
+        });
+
+        $this->assertCount(1, $newCollection);
+
+        $item = $newCollection->first();
+
+        $this->assertEquals('john', $item);
+    }
+
+    /** @test */
+    public function a_reject_accepts_both_key_and_value()
+    {
+        $collection = new Collection([
+            'name' => 'john',
+            'age'  => 35,
+            'sex'  => 'male'
+        ]);
+
+        $newCollection = $collection->reject(function ($value, $key) {
+            return $key == 'name' && $value == 'john';
+        });
+
+        $this->assertCount(2, $newCollection);
+
+        $item = $newCollection->shift();
+        $this->assertEquals(35, $item);
+
+        $item = $newCollection->shift();
+        $this->assertEquals('male', $item);
+    }
+
+    /** @test */
+    public function it_takes_exception_to_passing_to_many_arguments_to_filter()
+    {
+        $collection = new Collection([
+            'some_item',
+            'some_other_item',
+            'some_third_item'
+        ]);
+
+        try {
+            $collection->filter(function ($value, $key, $thisShouldNotBeHere) {
+                // This codes should never be executed
+                // It is only here to satisfy IDEs
+                // and tools like PHPStan
+                $notRelevant = [
+                    $value,
+                    $key,
+                    $thisShouldNotBeHere
+                ];
+
+                return $notRelevant;
+            });
+        } catch (Exception $exception) {
+            $this->assertInstanceOf(RuntimeException::class, $exception);
+            $this->assertEquals('Too many parameters for filter() callback', $exception->getMessage());
+        }
+    }
+
+    /** @test */
+    public function it_takes_exception_to_passing_to_many_arguments_to_reject()
+    {
+        $collection = new Collection([
+            'some_item',
+            'some_other_item',
+            'some_third_item'
+        ]);
+
+        try {
+            $collection->reject(function ($value, $key, $thisShouldNotBeHere) {
+                // This codes should never be executed
+                // It is only here to satisfy IDEs
+                // and tools like PHPStan
+                $notRelevant = [
+                    $value,
+                    $key,
+                    $thisShouldNotBeHere
+                ];
+
+                return $notRelevant;
+            });
+        } catch (Exception $exception) {
+            $this->assertInstanceOf(RuntimeException::class, $exception);
+            $this->assertEquals('Too many parameters for reject() callback', $exception->getMessage());
+        }
+    }
+
+    /** @test */
+    public function it_can_check_every_item_against_a_truth_test()
+    {
+        $collectionA = new Collection([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        $resultA = $collectionA->every(function ($item) {
+            return is_integer($item);
+        });
+
+        $this->assertTrue($resultA);
+
+        $collectionB = new Collection([1, 'two', 3, 'four', 5, 'six', 7, 'eight', 9]);
+        $resultB = $collectionB->every(function ($item) {
+            return is_integer($item);
+        });
+
+        $this->assertFalse($resultB);
+    }
+
+    /** @test */
+    public function it_can_reduce_a_collection_to_a_single_value()
+    {
+        $collectionA = new Collection([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+        // Implementation of sum() using reduce()
+        $sum = $collectionA->reduce(function ($carry, $item) {
+            $carry += $item;
+
+            return $carry;
+        }, 0);
+
+        $this->assertEquals(45, $sum);
+    }
+
+    /** @test */
+    public function it_can_summarize_a_collection_of_numbers()
+    {
+        $collection = new Collection([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+        $this->assertEquals(45, $collection->sum());
+    }
+
+    /** @test */
+    public function it_can_summarize_a_collection_of_items_using_a_specific_field()
+    {
+        $collection = new Collection([
+            [
+                'name' => 'john',
+                'age'  => 30
+            ],
+            [
+                'name' => 'jane',
+                'age'  => 26
+            ],
+            [
+                'name' => 'jacob',
+                'age'  => 44
+            ],
+        ]);
+
+        $this->assertEquals(100, $collection->sum('age'));
+    }
+}
